d557cde95e99d02b11fc882efd588ad959dbe58b
FAkperovs <farruhs-akperovs@inbox.lvgit remote add origin https://github.com/FAkperovs/html.gitgit branch -M mastergit push -u origin mastergit show branchgit show - branchgit branch>
2022-11-23 20:43:28 +0200

    [master]: Add classroom progress 23.11

--- src/test/java/TestChrome.java
+++ src/test/java/TestChrome.java
@@ -2,12 +2,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import static lv.acodemy.constants.Generic.GOOGLE_URL;
+import static org.testng.Assert.assertEquals;
+
 public class TestChrome {
-    ChromeDriver driver = new ChromeDriver() ;
+
+    ChromeDriver driver = new ChromeDriver();
 
     @AfterMethod
     public void tearDown() {
@@ -17,9 +20,9 @@ public class TestChrome {
 
     @Test
     public void chromeTest() {
-        driver.get("https://google.lv/?hl=en");
+        driver.get(GOOGLE_URL);
 
-        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')] "));
+        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
         acceptButton.click();
 
         WebElement searchField = driver.findElement(By.name("q"));
@@ -27,10 +30,21 @@ public class TestChrome {
 
         searchField.sendKeys(Keys.ENTER);
 
-        Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
-
-
+        assertEquals(driver.getTitle(), "acodemy - Google Search");
     }
 
+    @Test
+    public void chromeTest2() {
+        driver.get(GOOGLE_URL);
 
+        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
+        acceptButton.click();
+
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("acodemy");
+
+        searchField.sendKeys(Keys.ENTER);
+
+        assertEquals(driver.getTitle(), "acodemy - Google Search");
+    }
 }
